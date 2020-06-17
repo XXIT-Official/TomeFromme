32bd997c07f9111382c1a37739010f1da6b474f3
j2de <j2de.dev@gmail.com>
2020-06-18 00:40:26 +0900

    feat: pop-up display settings

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -5,3 +5,8 @@ function setCookie(cookie_name, value, days) {
     var cookie_value = escape(value) + ((days == null) ? '' : '; expires =' + exdate.toUTCString() + '; SameSite=Strict');
     document.cookie = cookie_name + '=' + cookie_value;
 }
+
+var getCookie = function(name) {
+    var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return value? value[2] : null;
+};
--- public/js/popup.js
+++ public/js/popup.js
@@ -1,5 +1,13 @@
 var close_button = document.getElementById("pop_up_close_button");
 
+window.onload = function() {
+    var popup_class = document.querySelector('.pop_up__layout');
+    // 쿠키 확인 
+    if (getCookie('popup_close') !== 'Y') { 
+        popup_class.classList.remove('hidden'); 
+    }
+};
+
 close_button.onclick = function() {
     var body = close_button.parentElement;
     var layout = body.parentElement;
