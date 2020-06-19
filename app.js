a8815ca799d7b387548e3e5f041d825e84025db2
MiryangJung <jung@miryang.dev>
2020-06-19 22:35:04 +0900

    Edit error page & Add loading

--- app.js
+++ app.js
@@ -32,14 +32,14 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('index');
 });
 
 module.exports = app;
